5cddf79c8cc06aa63b782ea5ecc01863ba2323d1
JayyKashtaCodes <jtman112@gmail.com>
2024-07-09 11:01:31 +1200

    Initial

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="main.css">
+        <link rel="preload" as="script" href="main.js">
+        <link rel="preload" as="script" href="https://code.jquery.com/jquery-latest.min.js">
+        <link rel="preload" as="script" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
+        <link rel="preload" as="script" href="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
+        <link rel="preload" as="script" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.3/jquery-ui.min.js">
+        <link rel="preload" as="script" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/min/dropzone.min.js">
+        <link rel="preload" as="script" href="https://cdn.jsdelivr.net/npm/jquery-validation@1.20.1/dist/jquery.validate.min.js">
+        <script>
+            function googleTranslateElementInit() {
+                new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
+            }
+        </script>
+    </head>
+    <body>
+        <video autoplay loop muted poster="https://img.freepik.com/free-photo/digital-art-beautiful-mountains_23-2151123686.jpg">
+            <source src="https://videos.pexels.com/video-files/3163534/3163534-uhd_2560_1440_30fps.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+        <div class="navbar">
+                <a href="./">Home</a>
+                <div class="dropdown">
+                    <a class="dropbtn">Mods</a>
+                    <div class="dropdown-content">
+                        <a href="#mod1">Mod 1</a>
+                        <a href="#mod2">Mod 2</a>
+                        <a href="#mod3">Mod 3</a>
+                        <a href="#mod4">Mod 4</a>
+                        <a href="#mod5">Mod 5</a>
+                        <a href="#mod6">Mod 6</a>
+                    </div>
+                </div> 
+                <div class="dropdown">
+                    <a class="dropbtn">Games</a>
+                    <div class="dropdown-content">
+                        <a href="#game1">Game 1</a>
+                    </div>
+                </div>
+                <a href="#contact">Contact</a>
+                <a href="#placehold">placeholder</a>
+                <div class="languageList" id="google_translate_element"></div>
+        </div>
+        <div class="content">
+        </div>
+        <script src="https://code.jquery.com/jquery-latest.min.js"></script>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.3/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/min/dropzone.min.js"></script>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery-validation@1.20.1/dist/jquery.validate.min.js"></script>
+        <script src="main.js"></script>
+    </body>
+</html>
