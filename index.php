647940c65f4836ca1a3e22a4a13f35b6421ed712
JayyKashtaCodes <jtman112@gmail.com>
2024-07-09 11:19:35 +1200

    Update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Home</title>
+        <title>Your Website Title</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="main.css">
